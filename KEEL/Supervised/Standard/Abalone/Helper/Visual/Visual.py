a5eff21a1057be905ab3e15531bd2e2f295e96e4
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-07-26 16:46:50 -0400

    test mkProject.sh

---
+++ KEEL/Supervised/Standard/Abalone/Helper/Visual/Visual.py
@@ -0,0 +1,29 @@
+from Helper.Load.load import Base_Load; import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+
+
+class Visual:
+    def __init__(self, Load):
+        self.load = Load
+        self.mpl_config()
+        self.construct_figures()
+
+    def mpl_config(self):
+        plt.rcParams["figure.figsize"] = (20,10)
+
+    def construct_figures(self):
+        self.make_scatter_matrix()
+        self.make_box()
+
+    def make_box(self):
+        fig, ax = plt.subplots()
+        self.load.data()[self.load.inputs].plot(kind='box', subplots=False, sharex=True, sharey=True)
+        plt.savefig("Data/Visual/box.png", bbox_inches='tight')
+        plt.close(fig)
+
+    def make_scatter_matrix(self):
+        fig, ax = plt.subplots()
+        scatter_matrix(self.load.data()[self.load.inputs], diagonal='kde')
+        plt.savefig("Data/Visual/scatter_matrix.png", bbox_inches='tight')
+        plt.close(fig)
+
---
+++ KEEL/mkProjectDirectory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+name=$1;dat=$2;txt=$3;path=$4;
+
+mkdir $path$name;
+mkdir $path$name/Data;
+mkdir $path$name/Data/Visual;
+mv $dat $path$name/Data/$dat;
+mv $txt $path$name/Data/$txt;
+cp -r Helper/ $path$name/Helper/
+cd $path$name; touch __init__.py; unzip Data/$dat
