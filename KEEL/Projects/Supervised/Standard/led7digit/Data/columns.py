fd38fcf38a87e2d15dd7a57e01ff7ffed1f919bd
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-08-04 00:00:53 -0400

    Work on building up column transformers. Consider how to scale up such a file when there are >> columns than 7.

--- KEEL/Projects/Supervised/Standard/led7digit/Data/columns.py
+++ KEEL/Projects/Supervised/Standard/led7digit/Data/columns.py
@@ -1,1 +1,34 @@
-Column_pipeline_Dictionary = {}
+from Helper.Transform.Selector.NumberSelector import NumberSelector
+from Helper.Transform.Selector.TextSelector import TextSelector
+from Helper.Transform.Columns.column_pipeline import column_pipeline
+
+from Helper.Transform.Custom.nominal import _LabelBinarizer
+
+_Led1 = {}
+_Led2 = {}
+_Led3 = {}
+_Led4 = {}
+_Led5 = {}
+_Led6 = {}
+_Led7 = {}
+_number = {}
+
+Led1 = column_pipeline("Led1", NumberSelector, _LabelBinarizer, _Led1)
+Led2 = column_pipeline("Led2", NumberSelector, _LabelBinarizer, _Led2)
+Led3 = column_pipeline("Led3", NumberSelector, _LabelBinarizer, _Led3)
+Led4 = column_pipeline("Led4", NumberSelector, _LabelBinarizer, _Led4)
+Led5 = column_pipeline("Led5", NumberSelector, _LabelBinarizer, _Led5)
+Led6 = column_pipeline("Led6", NumberSelector, _LabelBinarizer, _Led6)
+Led7 = column_pipeline("Led7", NumberSelector, _LabelBinarizer, _Led7)
+number = column_pipeline("Number", NumberSelector, _LabelBinarizer, _number)
+
+Column_pipeline_Dictionary = {
+        "Led1":Led1,
+        "Led2":Led2,
+        "Led3":Led3,
+        "Led4":Led4,
+        "Led5":Led5,
+        "Led6":Led6,
+        "Led7":Led7,
+        "Number":number
+        }
